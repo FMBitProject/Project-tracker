a0bb25c0f65bad26a44ef12af46eb6fe40e0ae56
renfael6 <renfael6@gmail.com>
2026-04-06 04:26:46 +0000

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Empty config for now
+  allowedDevOrigins: [
+    "3000-firebase-project-tracker-1775093705756.cluster-cd3bsnf6r5bemwki2bxljme5as.cloudworkstations.dev"
+  ],
 };
 
 export default nextConfig;
